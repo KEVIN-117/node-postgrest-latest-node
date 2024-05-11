2b1043209f5c47ab46b2e3f6e0620c2819d52114
KEVIN-117 <krodriguezcondori34@gmai.com>
2024-05-11 17:20:20 -0400

    example jsx tester

--- tests/unit/jsx.test.js
+++ tests/unit/jsx.test.js
@@ -1,7 +1,7 @@
-import {describe, expect, test} from "@jest/globals";
+import { describe, expect, test } from '@jest/globals'
 
-describe('jsx tester' , () => {
-    test('should return <h1>hello</h1> when hello', () => {
-        expect(`<h1>hello</h1>`).toBe(`<h1>hello</h1>`)
-    })
+describe('jsx tester', () => {
+  test('should return <h1>hello</h1> when hello', () => {
+    expect(`<h1>hello</h1>`).toBe(`<h1>hello</h1>`)
+  })
 })
